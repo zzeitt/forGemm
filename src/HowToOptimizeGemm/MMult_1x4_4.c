f91390873f1c752ae25778a9345af360ddefb103
ztMelete <zhongtao@hygon.cn>
2023-08-18 14:23:50 +0800

    add: MMult_1x4_4-AddDot() "inline"

---
+++ src/HowToOptimizeGemm/MMult_1x4_4.c
@@ -0,0 +1,42 @@
+/* Create macros so that the matrices are stored in column-major order */
+
+#define A(i,j) a[ (j)*lda + (i) ]
+#define B(i,j) b[ (j)*ldb + (i) ]
+#define C(i,j) c[ (j)*ldc + (i) ]
+
+/* Routine for computing C = A * B + C */
+
+void AddDot(int, double*, int, double*, double*);
+void AddDot1x4(int, double*, int, double*, int, double*, int);
+void MY_MMult( int m, int n, int k, double *a, int lda, 
+                                    double *b, int ldb,
+                                    double *c, int ldc )
+{
+  int i, j;
+
+  for ( i=0; i<m; i++ ){        /* Loop over the rows of C */
+    for ( j=0; j<n; j+=4 ){        /* Loop over the columns of C */
+      /* Update C(i, j), C(i, j+1), C(i, j+2), C(i, j+3)*/
+      AddDot1x4(k, &A(i, 0), lda, &B(0, j), ldb, &C(i, j), ldc);
+    }
+  }
+}
+
+void AddDot1x4(int k, double* a, int lda, double* b, int ldb, double* c, int ldc) {
+  int p;
+  for (p = 0; p < k; p++) {
+    C(0, 0) += A(0, p) * B(p, 0);
+  }
+  for (p = 0; p < k; p++)
+  {
+    C(0, 1) += A(0, p) * B(p, 1);
+  }
+  for (p = 0; p < k; p++)
+  {
+    C(0, 2) += A(0, p) * B(p, 2);
+  }
+  for (p = 0; p < k; p++)
+  {
+    C(0, 3) += A(0, p) * B(p, 3);
+  }
+}
