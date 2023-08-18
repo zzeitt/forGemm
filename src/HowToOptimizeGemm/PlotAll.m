e7e380b6de98aad243282481ad711ed3b36639f7
ztMelete <zhongtao@hygon.cn>
2023-08-18 19:27:01 +0800

    update: support $(PREV)

--- src/HowToOptimizeGemm/PlotAll.m
+++ src/HowToOptimizeGemm/PlotAll.m
@@ -18,6 +18,7 @@ max_gflops = nflops_per_cycle * nprocessors * GHz_of_processor;
 % Read in the first data set and plot it.
 %
 
+%%%%%%%%%%%%%%%%%%%%%%% old %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 output_old
 
 version_old = version;
@@ -32,18 +33,25 @@ axis( [ 0 MY_MMult( last,1 ) 0 max_gflops ] );
 xlabel( 'm = n = k' );
 ylabel( 'GFLOPS/sec.' );
 
-%
-% Read in second data set and plot it.
-%
+%%%%%%%%%%%%%%%%%%%%%%% prev %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+output_prev
 
+version_prev = version
+
+plot( MY_MMult( :,1 ), MY_MMult( :,2 ), 'g<-.;PREV;' );
+
+hold on
+
+%%%%%%%%%%%%%%%%%%%%%%% new %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 output_new
 
 version_new = version
 
-title_string = sprintf("OLD = %s, NEW = %s", version_old, version_new);
-
 plot( MY_MMult( :,1 ), MY_MMult( :,2 ), 'r-*;NEW;' );
 
+title_string = sprintf(
+    "OLD = %s, PREV = %s, NEW = %s", 
+    version_old, version_prev, version_new);
 title( title_string );
 
 filename = sprintf( "compare_%s_%s", version_old, version_new );
